a3f2965a22d42eba2b953ad7bfef4c201542bebb
Ketankharwar18 <ketankharwar18@gmail.com>
2026-08-03 23:49:40 +0530

    Added Bubble Sort

---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,54 @@
+package Sorting;
+
+import java.util.*;
+
+class BubbleSort {
+
+    
+    static void bubbleSort(int arr[], int n) {
+
+        for (int i = n - 1; i >= 1; i--) {
+
+            for (int j = 0; j < i; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+
+                    // Swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+
+        
+        bubbleSort(arr, n);
+
+
+        
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
