0fb7f1d5b819d334f21720c8d62559dcd04f324e
Ketankharwar18 <ketankharwar18@gmail.com>
2026-08-31 20:42:47 +0530

    Added  Largest Element Array

---
+++ Array/largestEle.java
@@ -0,0 +1,29 @@
+package Array;
+import java.util.*;
+public class largestEle {
+
+   static int largestEle(int arr[], int n) {
+        int max = arr[0];
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int largest = largestEle(arr, n);
+        System.out.println("Largest element in the array is: " + largest);
+        sc.close();
+    }
+    
+}
